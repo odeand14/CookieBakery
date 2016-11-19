ecd60826ecf7c8d5319dcca4abfd33bce35fd78c
Andreas <andreas@waadeland.no>
2016-11-19 01:25:32 +0100

    Added random bakery name

--- CookieBakery/CookieBakery/BaseCookie.cs
+++ CookieBakery/CookieBakery/BaseCookie.cs
@@ -1,16 +1,23 @@
-﻿namespace CookieBakery
+﻿using System;
+
+namespace CookieBakery
 {
     public class BaseCookie : ICookie
     {
         private readonly string _name;
         private readonly string _bakery;
         private readonly int _number;
+        private string[] Bakeries { get; set; } = { "Grandma's", "Peters Cookies", "Delicious dough", "Awesome Cookies", "Johnny's Bakery", "Cookie heaven", "Bake and shake", "Cake addict", "JKEA", "8-Twelve"};
+        private readonly Random random = new Random();
+        public string RandomBakery() {
+            return Bakeries[random.Next(0, Bakeries.Length)];
+        }
 
-        public BaseCookie(int number = 0, string name = "Cookie", string bakery = "Johnny's Bakery")
+        public BaseCookie(int number = 0, string name = "Cookie", string bakery = "")
         {
             _number = number;
             _name = name;
-            _bakery = bakery;
+            _bakery = (bakery.Equals("")) ? RandomBakery() : bakery;
         }
         public virtual string GetName()
         {
